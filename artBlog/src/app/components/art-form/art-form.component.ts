25c4292eac77d777e33bf6a882284cb39a060c72
Andrea <andrea.palanco@gmail.com>
2025-02-21 23:12:49 +0100

    Componentes e Interfaz
    He creado 2 componentes: artList y artForm, y la interfaz: IArt

---
+++ artBlog/src/app/components/art-form/art-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-art-form',
+  imports: [],
+  templateUrl: './art-form.component.html',
+  styleUrl: './art-form.component.css'
+})
+export class ArtFormComponent {
+
+}
