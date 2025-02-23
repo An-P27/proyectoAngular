9135c01e0a0016881c4062544b7cdb5a5ce0e3aa
Andrea <andrea.palanco@gmail.com>
2025-02-23 15:18:44 +0100

    Mejoras formulario
    He trabajado en mejoras sobre el formulario, valores no válidos y su estilo.

--- artBlog/src/app/components/art-form/art-form.component.ts
+++ artBlog/src/app/components/art-form/art-form.component.ts
@@ -1,19 +1,33 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { IArt } from '../../interfaces/iart.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-art-form',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './art-form.component.html',
   styleUrl: './art-form.component.css',
 })
 export class ArtFormComponent {
   newArt: IArt = { title: '', about: '', date: '', image: '' };
+
   @Output() sendArt: EventEmitter<IArt> = new EventEmitter();
 
+  @ViewChild('myForm') myForm!: NgForm;
+
   getArt() {
+    if (this.myForm.invalid) {
+      this.myForm.form.markAllAsTouched();
+    }
+
     this.sendArt.emit(this.newArt);
+
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.myForm.resetForm();
     this.newArt = { title: '', about: '', date: '', image: '' };
   }
 }
